0423a247a5db1b20011aea5a01acfaef085e5867
jijijiwonton <ji04wonton30@gmail.com>
2022-12-31 18:00:12 -0500

    Automatically sent email code done

--- automation/src/ContactUs.jsx
+++ automation/src/ContactUs.jsx
@@ -6,14 +6,32 @@ const ContactUs = () => {
   const changeHandler = (e) => {
     setFormState({ ...formState, [e.target.name]: e.target.value });
   };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const config = {
+      /**
+       * Refer to the video from 05:00
+       * https://www.youtube.com/watch?v=wUK40U6sPH0
+       */
+      SecureToken: '', // Needed for user name, password, server and port
+      To: '',
+      From: formState.email,
+      Subject: '',
+      Body: ``,
+    };
+    if (window.Email) {
+      window.Email.send(config).then(() => alert('Email sent successfully!'));
+    }
+  };
   return (
     <div>
-      <form className="container">
+      <form className="container" onSubmit={submitHandler}>
         <input
           type="text"
           name="name"
           placeholder="Enter your name please."
-          value=""
+          value={formState.name || ''}
           onChange={changeHandler}
           className="border"
         ></input>
@@ -21,7 +39,7 @@ const ContactUs = () => {
           type="email"
           name="email"
           placeholder="Enter your email please."
-          value=""
+          value={formState.email || ''}
           onChange={changeHandler}
           className="border"
         ></input>
